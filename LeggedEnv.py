b9bffc9fe90c43b46052a913d98d9408ed4136de
PraveenElango <praveenelango98@gmail.com>
2023-03-19 14:42:05 +0800

    Implementation of CPG controller for trotting gait (symmetric)

--- LeggedEnv.py
+++ LeggedEnv.py
@@ -27,12 +27,12 @@ class LeggedEnv(gym.Env):
 
         self.surface = Surface(
             texture_path="wood.png",
-            lateral_friction=1.0,
-            spinning_friction=1.0,
-            rolling_friction=0.01)
+            lateral_friction=5.0,
+            spinning_friction=5.0,
+            rolling_friction=0.0)
         
         self.spawn_robot()
-        self.spawn_surface()
+        # self.spawn_surface()
 
     def spawn_robot(self):
         """
@@ -60,13 +60,67 @@ class LeggedEnv(gym.Env):
         # Get the information of the actuator joints
         self.actuators_info = [p.getJointInfo(self.robot, joint) for joint in self.actuators]
 
+        # print(self.actuators_info)
+
+        self.start_joint_pos = [
+            0, #front left upper
+            0, #front left lower
+            0, #front right upper
+            0, #front right lower
+            0, #back left upper
+            0, #back left lower
+            0, #back right upper
+            0  #back right lower
+        ]
+
+        # Set the joint positions
+        for i in range(self.num_of_joints):
+            p.resetJointState(self.robot, self.actuators[i], self.start_joint_pos[i])
+        
         # Set all the motors to velocity control
-        p.setJointMotorControlArray(self.robot, self.actuators, controlMode=p.VELOCITY_CONTROL)
+        p.setJointMotorControlArray(self.robot, self.actuators, controlMode=p.POSITION_CONTROL)
 
-    def spawn_surface(self):
+        # Upper and lower joint indeces
+        self.upper_joint_indeces = [0, 2, 4, 6]
+        self.lower_joint_indeces = [1, 3, 5, 7]
 
-        while True:
-            p.stepSimulation()
+    def cpg_controller(self, t):
+        
+        # Set the CPG parameters
+        self.frequency = 3
+        self.amplitude = 0.3
+        self.phase_offset = 0.5
 
+        # Calculate the CPG output for each leg
+        front_left_leg_pos = self.amplitude * np.sin(
+            2 * np.pi * self.frequency * t + self.phase_offset)
+        front_right_leg_pos = self.amplitude * np.sin(
+            2 * np.pi * self.frequency * t + np.pi / 2 + self.phase_offset)
+        back_left_leg_pos = self.amplitude * np.sin(
+            2 * np.pi * self.frequency * t + np.pi + self.phase_offset)
+        back_right_leg_pos = self.amplitude * np.sin(
+            2 * np.pi * self.frequency * t + 3 * np.pi / 4 + self.phase_offset)
+
+        # Return the CPG output for all 4 legs
+        return [front_left_leg_pos, front_right_leg_pos, 
+                back_left_leg_pos, back_right_leg_pos]
+            
 if __name__ == "__main__":
     env = LeggedEnv()
+    # p.setRealTimeSimulation(1)
+    t = 0
+    while True:
+        desiredJointVelocities = [1, 1, 1, 1, 1, 1, 1, 1]
+        forces = [50] * len(env.actuators)
+        p.setJointMotorControlArray(env.robot, env.actuators, controlMode=p.POSITION_CONTROL, targetVelocities=env.start_joint_pos, forces=forces)
+        # p.setJointMotorControlArray(env.robot, env.actuators, controlMode=p.VELOCI TY_CONTROL, targetVelocities=desiredJointVelocities, forces= forces)
+        # for i in range(env.num_of_joints):
+        #     print(i, p.getJointState(env.robot, env.actuators[i]))
+        leg_positions = env.cpg_controller(t)
+        p.setJointMotorControlArray(env.robot, env.upper_joint_indeces, 
+                                    p.POSITION_CONTROL, targetPositions=-np.array(leg_positions))
+        p.setJointMotorControlArray(env.robot, env.lower_joint_indeces, 
+                                    p.POSITION_CONTROL, targetPositions=leg_positions)
+        p.stepSimulation()
+        time.sleep(0.01)
+        t += 0.01
