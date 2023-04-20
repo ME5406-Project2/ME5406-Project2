d033cb60c133bbb111ee9080a4c96412ccd58ea4
PraveenElango <praveenelango98@gmail.com>
2023-04-20 12:49:48 +0800

    Pure mud

--- LeggedEnv.py
+++ LeggedEnv.py
@@ -359,14 +359,15 @@ class LeggedEnv(gym.Env):
         
     def generate_terrain(self):
         # create a collision shape for the mud
-        half_size = [1.8, 2, 0.15]
+        half_size = [5, 2, 0.15]
         block_shape = p.createCollisionShape(p.GEOM_BOX, halfExtents=half_size)
 
         # create a multi-body object for the mud
-        if random.randint(0,1):
-            block_position = [4.5, 0, 0]
-        else:
-            block_position = [1, 0, 0]
+        # if random.randint(0,1):
+        #     block_position = [4.5, 0, 0]
+        # else:
+        #     block_position = [1, 0, 0]
+        block_position = [1, 0, 0]
         block_orientation = p.getQuaternionFromEuler([0, 0, 0])
         self.mud = p.createMultiBody(
             baseMass=0,
